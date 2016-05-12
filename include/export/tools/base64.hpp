e00b747d3b2cc58eb3bff2ab8cb382b2f7dd4fc5
Artur Troian <troian.ap@gmail.com>
2016-05-12 13:19:33 +0300

    Cleanup var names
    Change-Id: I50a7b705bdb463508072fe7fc1220a2c020a98df
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- include/export/tools/base64.hpp
+++ include/export/tools/base64.hpp
@@ -1,29 +1,27 @@
 /**
-   \file base64.h
-
-   Copyright (C) 2004-2008 Rene Nyffenegger
-
-   This source code is provided 'as-is', without any express or implied
-   warranty. In no event will the author be held liable for any damages
-   arising from the use of this software.
-
-   Permission is granted to anyone to use this software for any purpose,
-   including commercial applications, and to alter it and redistribute it
-   freely, subject to the following restrictions:
-
-   1. The origin of this source code must not be misrepresented; you must not
-      claim that you wrote the original source code. If you use this source code
-      in a product, an acknowledgment in the product documentation would be
-      appreciated but is not required.
-
-   2. Altered source versions must be plainly marked as such, and must not be
-      misrepresented as being the original source code.
-
-   3. This notice may not be removed or altered from any source distribution.
-
-   \author Rene Nyffenegger rene.nyffenegger@adp-gmbh.ch
-
-*/
+ * Copyright (C) 2004-2008 Rene Nyffenegger
+ *
+ * This source code is provided 'as-is', without any express or implied
+ * warranty. In no event will the author be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this source code must not be misrepresented; you must not
+ *    claim that you wrote the original source code. If you use this source code
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original source code.
+ *
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ * \author Rene Nyffenegger rene.nyffenegger@adp-gmbh.ch
+ * \author Artur Troian troian.ap@gmail.com
+ */
 
 #pragma once
 
@@ -54,7 +52,26 @@ public:
 	 * \return  None
 	 */
 	static void encode(std::string &b64, std::vector<uint8_t> &stream);
+
+	/**
+	 * \brief
+	 *
+	 * \param[out]
+	 * \param[in]
+	 *
+	 * \return None
+	 */
 	static void encode(std::string &b64, const std::string &stream);
+
+	/**
+	 * \brief
+	 *
+	 * \param[out]
+	 * \param[in]
+	 * \param[in]
+	 *
+	 * \return None
+	 */
     static void encode(std::string &b64, const uint8_t *stream, size_t in_len);
 
 	/**
@@ -65,7 +82,7 @@ public:
 	 * \return
 	 */
 	template<typename T>
-	static T decode(std::string const &stream) {
+	static T decode(const std::string &stream) {
 		int in_len = stream.size();
 		int i = 0;
 		int j = 0;
--- src/base64.cpp
+++ src/base64.cpp
@@ -1,29 +1,27 @@
 /**
-   \file base64.h
-
-   Copyright (C) 2004-2008 Rene Nyffenegger
-
-   This source code is provided 'as-is', without any express or implied
-   warranty. In no event will the author be held liable for any damages
-   arising from the use of this software.
-
-   Permission is granted to anyone to use this software for any purpose,
-   including commercial applications, and to alter it and redistribute it
-   freely, subject to the following restrictions:
-
-   1. The origin of this source code must not be misrepresented; you must not
-      claim that you wrote the original source code. If you use this source code
-      in a product, an acknowledgment in the product documentation would be
-      appreciated but is not required.
-
-   2. Altered source versions must be plainly marked as such, and must not be
-      misrepresented as being the original source code.
-
-   3. This notice may not be removed or altered from any source distribution.
-
-   \author Rene Nyffenegger rene.nyffenegger@adp-gmbh.ch
-
-*/
+ * Copyright (C) 2004-2008 Rene Nyffenegger
+ *
+ * This source code is provided 'as-is', without any express or implied
+ * warranty. In no event will the author be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this source code must not be misrepresented; you must not
+ *    claim that you wrote the original source code. If you use this source code
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original source code.
+ *
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ * \author Rene Nyffenegger rene.nyffenegger@adp-gmbh.ch
+ * \author Artur Troian troian.ap@gmail.com
+ */
 
 #include <iostream>
 #include <string>
@@ -41,19 +39,19 @@ void base64::encode(std::string &b64, const uint8_t *stream, size_t in_len)
 	int i = 0;
 	int j = 0;
 	int k = 0;
-	unsigned char char_array_3[3];
-	unsigned char char_array_4[4];
+	uint8_t array_3[3];
+	uint8_t array_4[4];
 
 	while (in_len--) {
-		char_array_3[i++] = stream[k++];
+		array_3[i++] = stream[k++];
 		if (i == 3) {
-			char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-			char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-			char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-			char_array_4[3] = char_array_3[2] & 0x3f;
+			array_4[0] = (array_3[0] & 0xfc) >> 2;
+			array_4[1] = ((array_3[0] & 0x03) << 4) + ((array_3[1] & 0xf0) >> 4);
+			array_4[2] = ((array_3[1] & 0x0f) << 2) + ((array_3[2] & 0xc0) >> 6);
+			array_4[3] = array_3[2] & 0x3f;
 
 			for (i = 0; (i < 4); i++) {
-				b64 += base64_chars[char_array_4[i]];
+				b64 += base64_chars[array_4[i]];
 			}
 			i = 0;
 		}
@@ -61,16 +59,16 @@ void base64::encode(std::string &b64, const uint8_t *stream, size_t in_len)
 
 	if (i) {
 		for (j = i; j < 3; j++) {
-			char_array_3[j] = '\0';
+			array_3[j] = '\0';
 		}
 
-		char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-		char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-		char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-		char_array_4[3] = char_array_3[2] & 0x3f;
+		array_4[0] = (array_3[0] & 0xfc) >> 2;
+		array_4[1] = ((array_3[0] & 0x03) << 4) + ((array_3[1] & 0xf0) >> 4);
+		array_4[2] = ((array_3[1] & 0x0f) << 2) + ((array_3[2] & 0xc0) >> 6);
+		array_4[3] = array_3[2] & 0x3f;
 
 		for (j = 0; (j < i + 1); j++) {
-			b64 += base64_chars[char_array_4[j]];
+			b64 += base64_chars[array_4[j]];
 		}
 
 		while ((i++ < 3)) {
